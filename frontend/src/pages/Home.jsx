66c9f331eac52b3c08fe2207c72368c1a6b8cf7a
manishsuthar94 <manishsutharr94@gmail.com>
2025-01-26 11:16:20 +0530

    mm

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import LatestCollection from '../components/LatestCollection'
-import BestSeller from '../components/BestSeller'
-import OurPolicy from '../components/OurPolicy'
-import NewsletterBox from '../components/NewsletterBox'
+import React from "react";
+import Hero from "../components/Hero";
+import LatestCollection from "../components/LatestCollection";
+import BestSeller from "../components/BestSeller";
+import OurPolicy from "../components/OurPolicy";
+import NewsletterBox from "../components/NewsletterBox";
+import { Helmet } from "react-helmet";
 
 const Home = () => {
   return (
-    <div>
-      <Hero />
-      <LatestCollection/>
-      <BestSeller/>
-      <OurPolicy/>
-      <NewsletterBox/>
-    </div>
-  )
-}
+    <>
+      <Helmet>
+        <title>Adaa Jaipur-Style Yourself With Adaa - AdaaJaipur</title>
+        <meta
+          name="description"
+          content="Adaa Jaipur-Style Yourself With Adaa - AdaaJaipur"
+        />
+      </Helmet>
+      <div>
+        <Hero />
+        <LatestCollection />
+        <BestSeller />
+        <OurPolicy />
+        <NewsletterBox />
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
